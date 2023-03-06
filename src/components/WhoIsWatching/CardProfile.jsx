659394dcf7b44056b05b036ffd9a0524655c52d9
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-03-05 19:03:45 -0600

    hows watching page ready

--- src/components/WhoIsWatching/CardProfile.jsx
+++ src/components/WhoIsWatching/CardProfile.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 function CardProfile(props) {
     return (
-        <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-            <img src={props.image} alt="Profile" className="object-cover w-full h-48" />
-            <div className="py-5 text-center">
-                <h3 className="text-lg font-semibold text-gray-800 dark:text-white">{props.name}</h3>
-                <span className="text-sm text-gray-700 dark:text-gray-400">{props.job}</span>
+        <div className="max-w-xs mx-auto">
+            <img src={props.image} alt="Profile" className="object-cover w-full h-48 rounded-lg shadow-md" />
+            <div className="py-3 text-center">
+                <h3 className="pt-1 text-lg font-semibold" style={{ color: "rgb(127, 127, 127)", fontSize: "150%" }}>{props.name}</h3>
+
             </div>
         </div>
     );
 }
 
 export default CardProfile;
+
+
--- src/components/WhoIsWatching/NavBar.jsx
+++ src/components/WhoIsWatching/NavBar.jsx
@@ -3,10 +3,11 @@ import netflixLogo from '../../assets/netflix-logo.svg';
 
 function NavBar() {
   return (
-    <nav className="flex items-center justify-between py-4 px-8 transparent-nav">
+    <nav className="flex items-center justify-between py-4 px-8 transparent-nav -mt-0 navbar">
       <img src={netflixLogo} alt="Netflix logo" className="h-8" />
     </nav>
   );
 }
+
 
 export default NavBar;
